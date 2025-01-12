08ceb5bc6b74198f1eab438603960959b1781e69
Deekshitha K <143080194+DeekshithaKPoojari@users.noreply.github.com>
2025-01-12 23:09:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // Make sure you're importing from 'react-dom/client'
+import ReactDOM from 'react-dom/client'; 
 
 import App from './App';
 
-// Create a root element and render the app using createRoot
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
